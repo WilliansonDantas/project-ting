30fb678d3811e371b28e8887011847773c582fd5
WilliansonDantas <williansondantas@gmail.com>
2023-03-08 16:05:44 -0300

    fix:corrige função txt_importer, com try e except

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,12 +4,11 @@ import sys
 
 def txt_importer(path_file):
     _, file_extension = os.path.splitext(path_file)
-    if not path_file:
-        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
     if file_extension != '.txt':
         return sys.stderr.write("Formato inválido\n")
-    else:
+    try:
         with open(path_file, 'r') as file:
             reader = file.read().split('\n')
-            print(reader)
         return reader
+    except:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
