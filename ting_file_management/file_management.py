3a6f92946aa0d4945beb23a3a8b1707acd0fec91
WilliansonDantas <williansondantas@gmail.com>
2023-03-08 16:02:48 -0300

    feat:função txt_importer,retorna conteúdo do file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -8,3 +8,8 @@ def txt_importer(path_file):
         return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
     if file_extension != '.txt':
         return sys.stderr.write("Formato inválido\n")
+    else:
+        with open(path_file, 'r') as file:
+            reader = file.read().split('\n')
+            print(reader)
+        return reader
