63e649e02fb64c730a9be49ab0ef4a2636e99b5d
WilliansonDantas <williansondantas@gmail.com>
2023-03-08 13:03:54 -0300

    feat:implementa o construtor da classe queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,8 +3,7 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        pass
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
         """Aqui irá sua implementação"""
