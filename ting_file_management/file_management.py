d290a7e6af9b2c5a430238406f43c2a0def7dcd5
WilliansonDantas <williansondantas@gmail.com>
2023-03-08 16:01:04 -0300

    feat:na função txt_importer,valida tipo do arquivo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,5 +3,8 @@ import sys
 
 
 def txt_importer(path_file):
+    _, file_extension = os.path.splitext(path_file)
     if not path_file:
         return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    if file_extension != '.txt':
+        return sys.stderr.write("Formato inválido\n")
