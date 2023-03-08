f23638bd4a44961e99609cbc5956fa5d9d4124bb
WilliansonDantas <williansondantas@gmail.com>
2023-03-08 13:06:11 -0300

    feat:implementa método dequeue, da classe Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -12,7 +12,7 @@ class Queue(AbstractQueue):
         self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.queue.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
