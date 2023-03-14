4b4bd65a529f688ca50953582ff55f838b4f27ef
WilliansonDantas <williansondantas@gmail.com>
2023-03-14 16:13:39 -0300

    feat:implementa função process, req03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    pathProcess = txt_importer(path_file)
+    outPut = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(pathProcess),
+        "linhas_do_arquivo": pathProcess
+    }
+    queueInstance = instance.queue
+    for item in queueInstance:
+        if item["nome_do_arquivo"] == path_file:
+            return None
+    instance.enqueue(outPut)
+    print(outPut)
 
 
 def remove(instance):
