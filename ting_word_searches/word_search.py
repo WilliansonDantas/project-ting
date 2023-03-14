d40659692874ec54fb94af10e26d679ae7c909ae
WilliansonDantas <williansondantas@gmail.com>
2023-03-14 17:38:03 -0300

    feat:solução da função exists_word, em word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    queueInstance = instance.queue
+    wordCaseInsensitive = word.lower()
+    wordResult = []
+    for file in queueInstance:
+        lines = []
+        for index, item in enumerate(file["linhas_do_arquivo"]):
+            itemCaseInsensitive = item.lower()
+            if wordCaseInsensitive in itemCaseInsensitive:
+                lines.append({
+                    "linha": index + 1
+                })
+        if len(lines) > 0:
+            wordResult.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": lines
+            })
+    return wordResult
 
 
 def search_by_word(word, instance):
