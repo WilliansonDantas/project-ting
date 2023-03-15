09c0fb4f6eb5247fb72e03adaed85ae3dd48daa1
WilliansonDantas <williansondantas@gmail.com>
2023-03-14 23:02:23 -0300

    fix:corrige erros de lint em test_priority_queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -9,28 +9,45 @@ def test_basic_priority_queueing():
         instanceTest.search(99)
 
     instanceTest = PriorityQueue()
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_0.txt", "qtd_linhas": 4})
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_1.txt", "qtd_linhas": 6})
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_2.txt", "qtd_linhas": 3})
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_3.txt", "qtd_linhas": 9})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_0.txt",
+                          "qtd_linhas": 4})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_1.txt",
+                          "qtd_linhas": 6})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_2.txt",
+                          "qtd_linhas": 3})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_3.txt",
+                          "qtd_linhas": 9})
     assert len(instanceTest.high_priority) == 2
     assert len(instanceTest.regular_priority) == 2
 
     instanceTest = PriorityQueue()
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_0.txt", "qtd_linhas": 4})
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_1.txt", "qtd_linhas": 6})
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_2.txt", "qtd_linhas": 3})
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_3.txt", "qtd_linhas": 9})
-    assert instanceTest.dequeue() == {"nome_do_arquivo": "arquivo_test_0.txt", "qtd_linhas": 4}
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_0.txt",
+                          "qtd_linhas": 4})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_1.txt",
+                          "qtd_linhas": 6})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_2.txt",
+                          "qtd_linhas": 3})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_3.txt",
+                          "qtd_linhas": 9})
+    assert instanceTest.dequeue() == {"nome_do_arquivo": "arquivo_test_0.txt",
+                                      "qtd_linhas": 4}
     assert len(instanceTest.high_priority) == 1
     assert len(instanceTest.regular_priority) == 2
 
     instanceTest = PriorityQueue()
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_0.txt", "qtd_linhas": 4})
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_1.txt", "qtd_linhas": 6})
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_2.txt", "qtd_linhas": 3})
-    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_3.txt", "qtd_linhas": 9})
-    assert instanceTest.search(0) == {"nome_do_arquivo": "arquivo_test_0.txt", "qtd_linhas": 4}
-    assert instanceTest.search(1) == {"nome_do_arquivo": "arquivo_test_2.txt", "qtd_linhas": 3}
-    assert instanceTest.search(2) == {"nome_do_arquivo": "arquivo_test_1.txt", "qtd_linhas": 6}
-    assert instanceTest.search(3) == {"nome_do_arquivo": "arquivo_test_3.txt", "qtd_linhas": 9}
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_0.txt",
+                          "qtd_linhas": 4})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_1.txt",
+                          "qtd_linhas": 6})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_2.txt",
+                          "qtd_linhas": 3})
+    instanceTest.enqueue({"nome_do_arquivo": "arquivo_test_3.txt",
+                          "qtd_linhas": 9})
+    assert instanceTest.search(0) == {"nome_do_arquivo": "arquivo_test_0.txt",
+                                      "qtd_linhas": 4}
+    assert instanceTest.search(1) == {"nome_do_arquivo": "arquivo_test_2.txt",
+                                      "qtd_linhas": 3}
+    assert instanceTest.search(2) == {"nome_do_arquivo": "arquivo_test_1.txt",
+                                      "qtd_linhas": 6}
+    assert instanceTest.search(3) == {"nome_do_arquivo": "arquivo_test_3.txt",
+                                      "qtd_linhas": 9}
